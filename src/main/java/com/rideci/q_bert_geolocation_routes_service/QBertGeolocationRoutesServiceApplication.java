1b10d5e72d5d7dd1fde25ccd79b664fd3b050f5e
DanielPM <danpame1219@gmail.com>
2026-06-28 01:17:46 -0500

    add(chore): Project created

---
+++ src/main/java/com/rideci/q_bert_geolocation_routes_service/QBertGeolocationRoutesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.rideci.q_bert_geolocation_routes_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QBertGeolocationRoutesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QBertGeolocationRoutesServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rideci/q_bert_geolocation_routes_service/QBertGeolocationRoutesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideci.q_bert_geolocation_routes_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QBertGeolocationRoutesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
