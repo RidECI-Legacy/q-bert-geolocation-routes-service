0f94b7e6e49047300123d0c6a5eb58c81053cfda
DanielPM <danpame1219@gmail.com>
2026-07-05 03:02:50 -0500

    refact: service and usecases

--- src/main/java/com/rideci/q_bert_geolocation_routes_service/application/useCases/GetRouteUseCaseImpl.java
+++ src/main/java/com/rideci/q_bert_geolocation_routes_service/application/useCases/GetRouteUseCaseImpl.java
@@ -1,7 +1,6 @@
 package com.rideci.q_bert_geolocation_routes_service.application.useCases;
 
 import com.rideci.q_bert_geolocation_routes_service.application.annotation.UseCase;
-import com.rideci.q_bert_geolocation_routes_service.domain.exception.RouteNotFoundException;
 import com.rideci.q_bert_geolocation_routes_service.domain.model.Route;
 import com.rideci.q_bert_geolocation_routes_service.domain.ports.in.GetRouteUseCase;
 import com.rideci.q_bert_geolocation_routes_service.domain.ports.out.GeolocationRepositoryOutPort;
@@ -17,8 +16,7 @@ public class GetRouteUseCaseImpl implements GetRouteUseCase {
 
     @Override
     public Mono<Route> getRoute(String routeId) {
-        return geolocationRepositoryOutPort.findRouteById(routeId)
-                .switchIfEmpty(Mono.error(() -> new RouteNotFoundException(routeId)));
+        return geolocationRepositoryOutPort.findRouteById(routeId);
     }
 
 }
