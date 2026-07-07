2132b51265cc4f4fd8e015c13694547c2c5e4e1e
DanielPM <danpame1219@gmail.com>
2026-07-06 23:07:09 -0500

    fix: sonarcloud usecases package rename

--- src/main/java/com/rideci/q_bert_geolocation_routes_service/application/use_cases/UpdateRouteUseCaseImpl.java
+++ src/main/java/com/rideci/q_bert_geolocation_routes_service/application/use_cases/UpdateRouteUseCaseImpl.java
@@ -1,8 +1,8 @@
-package com.rideci.q_bert_geolocation_routes_service.application.useCases;
+package com.rideci.q_bert_geolocation_routes_service.application.use_cases;
 
 import com.rideci.q_bert_geolocation_routes_service.application.annotation.UseCase;
 import com.rideci.q_bert_geolocation_routes_service.domain.model.Route;
-import com.rideci.q_bert_geolocation_routes_service.domain.ports.in.CreateRouteUseCase;
+import com.rideci.q_bert_geolocation_routes_service.domain.ports.in.UpdateRouteUseCase;
 import com.rideci.q_bert_geolocation_routes_service.domain.ports.out.GeolocationRepositoryOutPort;
 
 import lombok.RequiredArgsConstructor;
@@ -10,12 +10,13 @@ import reactor.core.publisher.Mono;
 
 @UseCase
 @RequiredArgsConstructor
-public class CreateRouteUseCaseImpl implements CreateRouteUseCase {
+public class UpdateRouteUseCaseImpl implements UpdateRouteUseCase {
 
     private final GeolocationRepositoryOutPort geolocationRepositoryOutPort;
 
     @Override
-    public Mono<Route> createRoute(Route route) {
-        return geolocationRepositoryOutPort.save(route);
+    public Mono<Route> updateRoute(String routeId, Route updatedRoute) {
+        return geolocationRepositoryOutPort.update(routeId, updatedRoute);
     }
+
 }
